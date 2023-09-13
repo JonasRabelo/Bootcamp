06e114018fc0891d03152c05866802393e086119
JonasRabelo <jonas2mr01@gmail.com>
2023-09-13 19:17:17 -0300

    Inicio bootcamp Wex

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,12 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Jonas", "da Silva Rabelo");
+Pessoa p2 = new Pessoa(nome: "Estefânia", sobrenome: "da Conceição de Souza Rabelo");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
