79626d46c3748e6b64ec690190cad51a4d536e80
JonasRabelo <jonas2mr01@gmail.com>
2023-09-14 11:32:34 -0300

    Exceções e Coleções em C#

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -2,7 +2,151 @@
 using System.Globalization;
 using ExemploExplorando.Models;
 
-DateTime data = DateTime.Now;
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+estados.Add("PA", "Pará");
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+estados["SP"] = "SÃO PAULO";
+estados.Remove("BA");
+System.Console.WriteLine("--------------------");
+
+foreach (var item in estados)
+{
+    System.Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+string chave = "BA";
+// Verificando se a chave já existe
+if (estados.ContainsKey(chave))
+{
+    System.Console.WriteLine($"Valor existente: {chave}");
+}
+else
+{
+    System.Console.WriteLine($"Valor não existe. É seguro adicionar a chave.");
+}
+
+
+
+
+
+
+/*Stack<int> pilha = new Stack<int>();
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+pilha.Pop();
+
+pilha.Push(20);
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*Queue<int> fila = new Queue<int>();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+fila.Dequeue();
+fila.Enqueue(10);
+foreach (int item in fila)
+{
+    Console.WriteLine(item);
+}*/
+
+
+// new ExemploExcecao().Metodo1();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*try
+{
+string[] linhas = File.ReadAllLines(@"..\Arquivos\arquivoLeitura.txt");
+Console.WriteLine();
+foreach(string linha in linhas)
+{
+    Console.WriteLine(linha);
+}
+Console.WriteLine();
+}
+catch (FileNotFoundException e)
+{
+    Console.WriteLine("Ocorreu um erro na leitura do arquivo. Arquivo não encontrado: " + e.Message);
+}
+catch (DirectoryNotFoundException e)
+{
+    Console.WriteLine("Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Ocorreu uma exceção genérica: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Chegou até aqui.");
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*DateTime data = DateTime.Now;
 
 Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
 Console.WriteLine(data.ToShortDateString());
@@ -25,7 +169,7 @@ else
 {
     System.Console.WriteLine($"{dataString} não é uma data válida");
 }
-System.Console.WriteLine(date0);
+System.Console.WriteLine(date0);*/
 
 
 
