222526de3e5b0b74441eed2c1b21da6f534d1826
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2023-09-14 21:32:03 -0300

    Abstração e Encapsulamento em C#

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,71 @@
+﻿// Classe base (superclasse)
+public class Animal
+{
+    public string Nome { get; set; }
+    
+    public void EmitirSom()
+    {
+        Console.WriteLine("Som do animal.");
+    }
+}
+
+// Classe derivada (subclasse)
+public class Cachorro : Animal
+{
+    public void Latir()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using ExemploPOO.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.ExibirSaldo();
+
+c1.Sacar(500);
+
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+/*Pessoa p1 = new Pessoa();
+p1.Nome = "Jonas";
+p1.Idade = 25;
+
+p1.Apresentar();
+*/
